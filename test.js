a02ad99050ecb8b203354b1222ad17ba27f92cf1
sfpgmr <sfpgmr@example.com>
2018-08-05 17:07:44 +0900

    update

--- test.js
+++ test.js
@@ -4,9 +4,12 @@
  */
 
  // 整数は文字列の形で指定
-let numString = '-123';
+//let numString = '-9223372036854775807';
+//let numString = '9223372036854775809';
+let numString = '1233333333333334566';
 let numArray = [];
 let minus = false;
+// 数値文字列を分割して配列に保存する
 {
   let i = 0;
   for(const c of numString){
@@ -43,37 +46,51 @@ while(numArray.length > 0 || b > 15){
       ans.push(b >> 4);
       b = remind;
     } else {
-      ans.push(0);
+      ans.push(0 | 0);
       remind = b;
     }
   })
 
   // 頭の0をとる
-
   let i = 0;
   while(ans[i] == 0){
     ++i;
   }
   numArray = ans.slice(i);
-  // 16進文字列に変換する
   hex.unshift(remind);  
 }
 
-console.log(b,remind,hex,numArray);
+if(hex.length > 16){
+  throw new Error('64bit整数の範囲を超えています。');
+}
 
-if(minus){
-  if(hex.length < 16){
-    let l = 16 - hex.length;
-    while(l > 0){
-      hex.unshift(0);
-      --l;
-    }
+// 桁揃え（16桁に）
+if(hex.length < 16){
+  let l = 16 - hex.length;
+  while(l > 0){
+    hex.unshift(0);
+    --l;
   }
+}
+
+// マイナス値の処理
+if(minus){
   hex = hex.map(d=>d ^ 0xf);
   hex[15] += 1;
+  if(hex[15] > 15){
+    hex[15] = 0;
+    for(let i = 14;i >= 0;--i){
+      hex[i] += 1;
+      if(hex[i] < 16){
+        break;
+      } else {
+        hex[i] = 0;
+      }
+    }
+  }
+  hex[0] |= 0b1000;//sign bitを立てる 
 }
 //hex.push(b);
 
 console.log(hex.map(d=>d.toString(16)).join(''));
-// 58936e53d139afefabb2683f150b684045f15
 
